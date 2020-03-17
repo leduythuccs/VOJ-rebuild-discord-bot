7f0d104014b4bc665d9965ad5e2f603230fffe2e
Le Duy Thuc <leduykhongngu@gmail.com>
2020-03-17 10:22:55 +0700

    import helper

--- BotControlCommand.py
+++ BotControlCommand.py
@@ -6,6 +6,7 @@ import time
 import subprocess
 import textwrap
 import sys
+from helper import helper
 # Adapted from TLE sources.
 # https://github.com/cheran-senthil/TLE/blob/master/tle/cogs/meta.py#L15
 def git_history():
