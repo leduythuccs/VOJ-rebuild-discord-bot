86d40343dd3d9d48d7b1802f81f01a6bfea6bbbc
Le Duy Thuc <leduykhongngu@gmail.com>
2020-03-17 11:03:04 +0700

    Fix message

--- CoronaCommand.py
+++ CoronaCommand.py
@@ -49,7 +49,7 @@ class CoronaCommand(commands.Cog):
         t += table.Data('Cases', data['vietnam']['cases'], data['global']['cases'])
         t += table.Data('Recovered', data['vietnam']['recovered'], data['global']['recovered'])
         t += table.Data('deaths', data['vietnam']['deaths'], data['global']['deaths'])
-        msg = "Some statistics about corona case: \n" '```\n' + str(t) + '\n```'
+        msg = "Some statistics about total corona virus cases: \n" '```\n' + str(t) + '\n```'
 
         await ctx.send(msg)
     
