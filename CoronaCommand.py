41b2d9090c967166c939d2bde9df6ac3a9286562
Le Duy Thuc <leduykhongngu@gmail.com>
2020-03-20 22:08:32 +0700

    add loop

--- CoronaCommand.py
+++ CoronaCommand.py
@@ -9,6 +9,9 @@ API_PATH = 'https://code.junookyo.xyz/api/ncov-moh/data.json'
 class CoronaCommand(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.log_channel = None
+        self.cur = None
+        self.looper().start()
 
     def get_data(self):
         r = requests.get(API_PATH).json()
@@ -18,8 +21,40 @@ class CoronaCommand(commands.Cog):
  
     # @commands.Cog.listener()
     # async def on_ready(self):
-    #     pass
+    #     log_channel_id = int(os.getenv('DICORD_LOG_CHANNEL_ID'))
+    #     self.log_channel = self.bot.get_channel(log_channel_id)
 
+    @tasks.loop(minutes=5.0)
+    async def looper(self):
+        print("looping")
+        nxt = self.get_data()
+        if nxt is None:
+            return
+        pre_cases = int(self.cur['vietnam']['cases'])
+        nxt_cases = int(nxt['vietnam']['cases'])
+        pre_recovered = int(self.cur['vietnam']['recovered'])
+        nxt_recovered = int(nxt['vietnam']['recovered'])
+        pre_deaths = int(self.cur['vietnam']['deaths'])
+        nxt_deaths = int(nxt['vietnam']['deaths'])
+
+        msg = ""
+        if (pre_cases != nxt_cases):
+            msg += '{0} more new cases (from {1} to {2})'.format(nxt_cases - pre_cases, pre_cases, nxt_cases)
+        if (pre_recovered != nxt_recovered):
+            msg += '{0} more new recovered (from {1} to {2})'.format(nxt_recovered - pre_recovered, pre_recovered, nxt_recovered)
+        if (pre_deaths != nxt_deaths):
+            msg += '{0} more new deaths (from {1} to {2})'.format(nxt_deaths - pre_deaths, pre_deaths, nxt_deaths)
+        if len(msg) == 0:
+            return
+        self.cur = nxt
+        msg = "Vietnam just has:\n" + msg
+        await self.log_channel.send(msg)
+
+    @looper.before_loop
+    async def before_looper(self):
+        log_channel_id = int(os.getenv('DICORD_LOG_CHANNEL_ID'))
+        self.log_channel = self.bot.get_channel(log_channel_id)
+        self.cur = self.get_data()
     @commands.command(brief='Get cases in Vietnam or Global', usage="case [optional: global]")
     async def cases(self, ctx, *args):
         """ Get total cases had corona virus in Vietnam or global, default = Vietnam"""
