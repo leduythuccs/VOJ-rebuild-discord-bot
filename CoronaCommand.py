b72b2e581bc7887ac07b8836b50b12334a9f212e
Le Duy Thuc <leduykhongngu@gmail.com>
2020-03-20 22:31:32 +0700

    fix text in looper function

--- CoronaCommand.py
+++ CoronaCommand.py
@@ -12,6 +12,7 @@ class CoronaCommand(commands.Cog):
         self.log_channel = None
         self.cur = None
         self.looper.start()
+        self.index = 0
 
     def get_data(self):
         r = requests.get(API_PATH).json()
@@ -26,7 +27,8 @@ class CoronaCommand(commands.Cog):
 
     @tasks.loop(minutes=10.0)
     async def looper(self):
-        print("looping")
+        self.index += 1
+        print("looping " + str(self.index))
         nxt = self.get_data()
         if nxt is None:
             return
@@ -36,14 +38,17 @@ class CoronaCommand(commands.Cog):
         nxt_recovered = int(nxt['vietnam']['recovered'])
         pre_deaths = int(self.cur['vietnam']['deaths'])
         nxt_deaths = int(nxt['vietnam']['deaths'])
-
+        if (self.index == 1):
+            pre_cases = 0
+            pre_recovered = 0
+            pre_deaths = 0
         msg = ""
         if (pre_cases != nxt_cases):
-            msg += '{0} more new cases (from {1} to {2})'.format(nxt_cases - pre_cases, pre_cases, nxt_cases)
+            msg += '- {0} more new cases (from {1} to {2}).\n'.format(nxt_cases - pre_cases, pre_cases, nxt_cases)
         if (pre_recovered != nxt_recovered):
-            msg += '{0} more new recovered (from {1} to {2})'.format(nxt_recovered - pre_recovered, pre_recovered, nxt_recovered)
+            msg += '- {0} more new recovered (from {1} to {2}).\n'.format(nxt_recovered - pre_recovered, pre_recovered, nxt_recovered)
         if (pre_deaths != nxt_deaths):
-            msg += '{0} more new deaths (from {1} to {2})'.format(nxt_deaths - pre_deaths, pre_deaths, nxt_deaths)
+            msg += '- {0} more new deaths (from {1} to {2})\n.'.format(nxt_deaths - pre_deaths, pre_deaths, nxt_deaths)
         if len(msg) == 0:
             return
         self.cur = nxt
